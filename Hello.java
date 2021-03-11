978be8e074f6410b11235cb0df09bc5104924310
MichaelM <mercierm@plu.edu>
2021-03-11 15:45:04 -0800

    add Person to Hello class and print name to console.

--- Hello.java
+++ Hello.java
@@ -1,5 +1,8 @@
 public class Hello {
     public static void main(String[] args) {
         System.out.print("hello");
+        Person first = new Person("Tim");
+
+        System.out.print(first.name);
     }
 }
